1130fc680c9e578be6a7158129fa68cf1b87b594
Nitanon Rachapradit <mf.in.th@gmail.com>
2026-03-03 17:03:27 -0500

    Add IState and StateController

---
+++ BluePeanuts.TurboStates/StateController.cs
@@ -0,0 +1,29 @@
+﻿namespace BluePeanuts.TurboStates;
+
+public sealed class StateController
+{
+    private IState? _currentState;
+
+    public void SetState(IState? state)
+    {
+        _currentState?.Exit();
+
+        _currentState = state;
+        _currentState?.Enter();
+    }
+
+    public void EmptyState()
+    {
+        SetState(null);
+    }
+
+    public void Process(double delta)
+    {
+        _currentState?.Process(delta);
+    }
+
+    public void PhysicsProcess(double delta)
+    {
+        _currentState?.PhysicsProcess(delta);
+    }
+}
