95108adf188c590d6fb5c89e38fe97d10bc2b34b
Nitanon Rachapradit <mf.in.th@gmail.com>
2026-03-10 01:12:24 -0400

    Namespace fix

--- BluePeanuts.TurboStates.Tests/GodotCollections.cs
+++ BluePeanuts.TurboStates.Tests/GodotCollections.cs
@@ -1,5 +1,7 @@
 using twodog.xunit;
 
+namespace BluePeanuts.TurboStates.Tests;
+
 [CollectionDefinition("Godot", DisableParallelization = true)]
 public class GodotCollection : ICollectionFixture<GodotHeadlessFixture>
 {
